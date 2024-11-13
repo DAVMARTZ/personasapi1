445ed6baa93400bf08231cf5a584244511e9a606
DAVMARTZ <lumartinezacev0611@gmail.com>
2024-11-13 10:57:19 -0500

    a
    New Change

--- src/main/java/co/edu/luWeb/personasapi/domain/PersonaService.java
+++ src/main/java/co/edu/luWeb/personasapi/domain/PersonaService.java
@@ -15,13 +15,5 @@ import java.util.List;
  */
 public interface PersonaService {
 
-    List<Persona> listar();
-
-    Persona listarId(int id);
-
-    Persona add(Persona p);
-
-    Persona edit(Persona p);
-
-    Persona delete(int id);
+    
 }
